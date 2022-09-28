5793ad2c9c083689904d7b9cda375fcbf6052967
Adrian-uni <79143545+Adrian-uni@users.noreply.github.com>
2022-09-28 13:14:20 +0200

    rutas y bootsrtrap acabado

--- appUsers/src/app/app.module.ts
+++ appUsers/src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { LogoutPageComponent } from './components/logout-page/logout-page.component';
+import { UsersPageComponent } from './components/users-page/users-page.component';
+import { SignupPageComponent } from './components/signup-page/signup-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+
+import { UsersService } from './services/usersService/users-service.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const misRutas: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'}, 
+  {path:'login', component:LoginPageComponent},
+  {path:'signup', component:SignupPageComponent},
+  {path:'home', component:HomePageComponent},
+  {path:'logout', component:LogoutPageComponent},
+  {path:'users', component:UsersPageComponent},
+  {path: '**', component: ErrorPageComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    LogoutPageComponent,
+    UsersPageComponent,
+    SignupPageComponent,
+    HomePageComponent,
+    ErrorPageComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(misRutas),
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
