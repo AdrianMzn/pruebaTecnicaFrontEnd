415be36984ffb79dee4f80c8cf2521aeab510549
Adrian-uni <79143545+Adrian-uni@users.noreply.github.com>
2022-10-04 21:28:18 +0200

    filtrosde users ok

--- appUsers/src/app/components/users-page/users-page.component.ts
+++ appUsers/src/app/components/users-page/users-page.component.ts
@@ -11,6 +11,12 @@ import { UsersService } from 'src/app/services/usersService/users-service.servic
 export class UsersPageComponent implements OnInit {
 
   usuarios: any[] = [];
+  usuariosFilted: any[] = [];
+
+  tipos: string[] = [ 'Default users', 'Administrators'];
+
+  nameInput: string = '';
+  typeInput: string = 'All';
 
   constructor(private usersService : UsersService, private router: Router) { }
 
@@ -22,6 +28,8 @@ export class UsersPageComponent implements OnInit {
         this.usuarios.forEach((element,index)=>{
             if( element.email==sessionStorage.getItem('email') ) this.usuarios.splice(index,1);
         });
+
+        this.usuariosFilted = this.usuarios;
         console.log(this.usuarios);
         
       }), (error: any) => {
@@ -44,5 +52,26 @@ export class UsersPageComponent implements OnInit {
       return sessionStorage.getItem('email')?.includes('@hiberus') || false;
     }
     return true;
+  }
+
+  applyFilters(){
+    
+    switch (this.typeInput) {
+      case 'Default users':
+        this.usuariosFilted = this.usuarios.filter( user => !user['email'].includes('@hiberus') );
+        break;
+      case 'Administrators':
+        this.usuariosFilted = this.usuarios.filter( user => user['email'].includes('@hiberus') );
+        break;
+      default:
+        this.usuariosFilted = this.usuarios;
+        break;
+    }
+
+    if(this.nameInput != ''){
+      this.usuariosFilted = this.usuariosFilted.filter( user => ( user['name'].includes(this.nameInput) 
+                                                                  || user['surname'].includes(this.nameInput)
+                                                                  || user['email'].includes(this.nameInput) ) );
+    }
   }
 }
